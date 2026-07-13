56b8d0022039f2cbbc5acc89513035897c7a1566
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2026-07-13 23:13:04 +0800

    :lipstick: add sidebar collapsible
    reduce version font size to 12px

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -43,6 +43,7 @@ class AdminPanelProvider extends PanelProvider
             ->pages([
                 Dashboard::class,
             ])
+            ->sidebarCollapsibleOnDesktop()
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
             ->widgets([
             ])
--- resources/views/partials/version.blade.php
+++ resources/views/partials/version.blade.php
@@ -7,7 +7,7 @@
 
 @if (filled($releaseTag) && filled($repositoryUrl))
     <div
-        style="width: 100%; padding: 1rem 1.5rem; color: var(--gray-400); font-size: 14px; line-height: 20px; text-align: center;">
+        style="width: 100%; padding: 1rem 1.5rem; color: var(--gray-400); font-size: 12px; line-height: 20px; text-align: center;">
         <a href="{{ rtrim($repositoryUrl, '/') }}/releases/tag/{{ rawurlencode($releaseTag) }}" target="_blank"
             rel="noopener noreferrer" style="color: inherit; text-decoration: none;">
             {{ $releaseTag }}
@@ -15,7 +15,7 @@
     </div>
 @elseif (filled($refName) && filled($commitSha) && filled($repositoryUrl))
     <div
-        style="width: 100%; padding: 1rem 1.5rem; color: var(--gray-400); font-size: 14px; line-height: 20px; text-align: center;">
+        style="width: 100%; padding: 1rem 1.5rem; color: var(--gray-400); font-size: 12px; line-height: 20px; text-align: center;">
         <a href="{{ rtrim($repositoryUrl, '/') }}/commit/{{ rawurlencode($commitSha) }}" target="_blank"
             rel="noopener noreferrer" style="color: inherit; text-decoration: none;">
             Ver. {{ $refName }}&#64;{{ substr($commitSha, 0, 7) }}
