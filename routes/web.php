cf7074403e8310ea79b1bc5bd62b0b644fbe3ee9
fareez <iqmal3@outlook.com>
2026-07-23 21:09:22 +0800

    ✨ auto redirect to dashboard for authenticated users

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    if (auth()->check()) {
+        return redirect()->route('filament.admin.pages.dashboard');
+    }
+
     return view('welcome');
 });
 
